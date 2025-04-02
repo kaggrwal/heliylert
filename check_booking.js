0f76c8906dbf1c04333331371ba3532326592453
Kumar Aggarrwal <kaggrwal@gmail.com>
2025-04-02 19:48:44 +0530

    Update check_booking.js

--- check_booking.js
+++ check_booking.js
@@ -1,24 +1,18 @@
-const puppeteer = require('puppeteer');
+const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await puppeteer.launch({
+    const browser = await chromium.launch({
         headless: true,
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage'
+            '--disable-setuid-sandbox'
         ]
     });
-
     const page = await browser.newPage();
     await page.goto('https://www.heliyatra.irctc.co.in/', { waitUntil: 'load' });
 
-    // Check if the "Book Ticket" button is enabled
-    const isBookingOpen = await page.evaluate(() => {
-        const button = document.querySelector('button');
-        return button && !button.disabled;
-    });
-
+    // Check for the "Book Ticket" button status
+    const isBookingOpen = await page.$eval('button', button => !button.disabled);
     console.log(isBookingOpen ? '🚨 Booking is OPEN! 🚨' : '❌ Booking is still CLOSED.');
 
     await browser.close();
